f2a62223fdcddb4fb3877fd9cc395e9c9b4c6463
hanachi-ap <hanachi.ap@gmail.com>
2022-08-20 17:46:59 +0900

    modify: firmware

--- firmware/qmk_firmware/keyboards/hanachi_ap/ergotonic_f24/config.h
+++ firmware/qmk_firmware/keyboards/hanachi_ap/ergotonic_f24/config.h
@@ -64,8 +64,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_DI_PIN D2
 #ifdef RGB_DI_PIN
-#    define RGBLED_SPLIT {8, 8}
-#    define RGBLED_NUM 16
+#    define RGBLED_SPLIT {9, 9}
+#    define RGBLED_NUM 18
 #    define RGBLIGHT_HUE_STEP 8
 #    define RGBLIGHT_SAT_STEP 8
 #    define RGBLIGHT_VAL_STEP 8
@@ -74,13 +74,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*== all animations enable ==*/
 //#    define RGBLIGHT_ANIMATIONS
 /*== or choose animations ==*/
-//#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_BREATHING
 #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//#    define RGBLIGHT_EFFECT_SNAKE
-//#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
 #    define RGBLIGHT_EFFECT_CHRISTMAS
-//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 #    define RGBLIGHT_EFFECT_RGB_TEST
 
 //#    define RGBLIGHT_EFFECT_ALTERNATING
